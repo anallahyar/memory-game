0c5a2bbc75f2f1c1947bf48f2b16ad67d057f22f
Arianna <anallahyar@gmail.com>
2017-09-26 14:48:29 +0200

    using state for cards

--- src/game.js
+++ src/game.js
@@ -1,27 +1,39 @@
 import React from "react"
 import Card from "./card"
-import Counter from "./counter"
 
-const Game = () => (
-  <div className='game'>
-    <h1><center>memory</center>
-    <br/>
-    <div className = 'subhead'><center>match the modes to win!</center></div></h1>
-      <Counter />
-      <Card src='/images/bus.jpg'/>
-      <Card src='/images/ferry.jpg'/>
-      <Card src='/images/metro.jpg'/>
-      <Card src='/images/cyclist.jpg'/>
-      <Card src='/images/sj.jpg'/>
-      <Card src='/images/drivenow.jpg'/>
-      <Card src='/images/bus.jpg'/>
-      <Card src='/images/ferry.jpg'/>
-      <Card src='/images/metro.jpg'/>
-      <Card src='/images/cyclist.jpg'/>
-      <Card src='/images/sj.jpg'/>
-      <Card src='/images/drivenow.jpg'/>
-  </div>
-)
+class Game extends React.Component {
+  state = {
+    cards: [
+        { src: '/images/bus.jpg' },
+        { src: '/images/ferry.jpg' },
+        { src: '/images/metro.jpg' },
+        { src: '/images/cyclist.jpg' },
+        { src: '/images/sj.jpg' },
+        { src: '/images/drivenow.jpg' },
+        { src: '/images/bus.jpg' },
+        { src: '/images/ferry.jpg' },
+        { src: '/images/metro.jpg' },
+        { src: '/images/cyclist.jpg' },
+        { src: '/images/sj.jpg' },
+        { src: '/images/drivenow.jpg' }
+      ]
+}
+
+  render () {
+    return (
+          <div>
+              <h1><center>memory</center>
+              <br/>
+              <div className = 'subhead'><center>match the modes to win!</center></div></h1>
+// use curly brackets to run JavaScript
+              {this.state.cards.map((card) => (
+                <Card src={card.src} />
+              ))}
+
+          </div>
+      )
+  }
+}
 
 export default Game //tells us what can we get, defines the default export
 
