ef58f3bb77879ea042a9f1d95db063d1252e685f
Arianna <anallahyar@gmail.com>
2017-09-27 11:51:16 +0200

    notes

--- src/card.js
+++ src/card.js
@@ -1,13 +1,25 @@
-import React from "react"
-import './card.css';
+import React from "react" // we need react to use react
+import './card.css'; // just normal css, react injects into the <head> component
 
+// defining a "Card" component.
+// That component is inheriting functions from React.Component (constructor, props)
 class Card extends React.Component {
 
+// the function we call from onClick. We give it a name 'handleClick' and takes no arguments
+// Because it's in this class, it has access to 'this' so it can use this.props.src
   handleClick = () => {
     alert('I have been clicked')
   }
 
-  render () {
+// render() is a required function for our component
+// React will invoke this function when it mounts the component
+  render () { // render function definition. NOT using arrow syntax
+    // The render needs to return JSX.
+    // JSX is basically HTML but you can use components as well
+    // for example, when we render this card, we do so in the Game component by using <Card />
+    //
+    // props become an object, so className="foo" becomes { className: "foo"}
+    // on this.props and onClick becomes { onClick: () => {} }
     return (
       <div onClick = {this.handleClick} className='card'>
        <img src={this.props.src} />
@@ -16,6 +28,7 @@ class Card extends React.Component {
   }
 }
 
+// to be able to use 'import Card from "./card",  we need to export it
 export default Card
 
 // {} allow us to write JavaScript
